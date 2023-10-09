445eeb602d16c22ccf5a2d93ceba2f15aa03230b
AntonioContrerasC <acontrecar@gmail.com>
2023-10-09 12:32:40 +0200

    Counter

--- src/app/shared/pages/nav-side-bar/nav-side-bar.component.ts
+++ src/app/shared/pages/nav-side-bar/nav-side-bar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, inject, signal } from '@angular/core';
 import { Router } from '@angular/router';
 import { ProductsService } from 'src/app/products/services/products.service';
+import { GlobalService } from '../../services/global.service';
+import { ShoppingCart } from '../../interfaces/carts.interfaces';
 
 @Component({
   selector: 'nav-side-bar',
@@ -12,14 +14,20 @@ export class NavSideBarComponent implements OnInit {
 
   private productsService = inject(ProductsService);
   private router = inject(Router);
+  private globalService = inject(GlobalService);
 
   public categories = signal<string[]>([]);
-
   public isCartOpen = signal<boolean>(false);
+
+  public numberOfProducts: number = 0;
 
   ngOnInit(): void {
     this.productsService.getAllCategories().subscribe((categories) => {
       this.categories.set(categories);
+    });
+
+    this.globalService.shoppingCart$.subscribe((products: ShoppingCart) => {
+      this.numberOfProducts = products.totalItems;
     });
   }
 
